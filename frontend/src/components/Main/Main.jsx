96c404a9fe1be8c59ba1005f468d3bfb1ebdd175
Octavio Salas <salasoctavio129@gmail.com>
2023-07-29 18:30:25 -0300

    deploThree

--- frontend/src/components/Main/Main.jsx
+++ frontend/src/components/Main/Main.jsx
@@ -51,7 +51,7 @@ export default function Main() {
             style={{ clipPath: 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)' }}  />
         </div>
 
-        <div className="mx-auto max-w-2xl py-32 py-16 sm:py-24 lg:py-32">
+        <div className="mx-auto max-w-2xl py-32  sm:py-24 lg:py-32">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
               <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
                    Want to know more about us?.{' '}
