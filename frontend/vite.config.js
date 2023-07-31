122457f9e5c35733d148d370556825e61bd64060
Octavio Salas <salasoctavio129@gmail.com>
2023-07-31 15:55:32 -0300

    dpel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import mongoose from 'mongoose';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    function() {
-      mongoose 
-    },
-  ],
+  plugins: [react()],
 })
